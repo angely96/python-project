2dcda3e41dbe940d6803d6e6d679f5bcb545638b
Aracelly <angelyaracelly321@gmail.com>
2025-08-22 12:51:10 -0500

    Creacion y conexion de la BD

--- banking/settings.py
+++ banking/settings.py
@@ -74,9 +74,20 @@ WSGI_APPLICATION = 'banking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST':'localhost',
+        'NAME': 'banking',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'PORT':'5432'
+        
+    },
+    
+    'local':{
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'banking.sqlite3',
     }
+    
 }
 
 
